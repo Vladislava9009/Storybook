06749cff7bb130dcd6288a5e5a231a729e293698
Vladislava <vladislava.bogaturova@gmail.com>
2021-05-24 13:04:37 +0300

    feat: add auto story loadfer

---
+++ storybook/storyLoader.js
@@ -0,0 +1,15 @@
+// Auto-generated file created by react-native-storybook-loader
+// Do not edit.
+//
+// https://github.com/elderfo/react-native-storybook-loader.git
+
+function loadStories() {
+  require('../src/stories/button/button.stories');
+}
+
+const stories = ['../src/stories/button/button.stories'];
+
+module.exports = {
+  loadStories,
+  stories,
+};
